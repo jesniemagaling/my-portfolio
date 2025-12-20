192d63fca9c3e9d35ed949f54b2d190eef7a2151
Jesnie <jesniemagaling05@gmail.com>
2025-12-20 14:09:49 +0800

    fix: admin login page dark theme

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -40,11 +40,12 @@ export default function AdminLoginPage() {
 
   return (
     <div className="flex items-center justify-center h-full">
-      <div className="w-full max-w-md p-10 bg-white border border-gray-200 shadow-lg rounded-2xl">
-        <h1 className="mb-2 text-3xl font-extrabold text-center text-gray-900">
+      <div className="w-full max-w-md p-10 rounded-2xl border bg-primary-light dark:bg-primary-dark border-gray-300 dark:border-[rgba(255,255,255,0.06)] shadow-lg">
+        <h1 className="mb-2 text-3xl font-extrabold text-center text-black dark:text-white">
           Admin
         </h1>
-        <p className="mb-6 text-center text-gray-500">
+
+        <p className="mb-6 text-center text-gray-500 dark:text-gray-400">
           Enter your admin password to access
         </p>
 
@@ -55,7 +56,9 @@ export default function AdminLoginPage() {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
-            className="w-full rounded-md border-2 border-gray-300 bg-transparent px-3 py-2 text-sm placeholder-[#7E7D7E] focus:outline-none focus:border-gray-600 dark:border-[rgba(255,255,255,0.06)] dark:focus:border-gray-400"
+            className="w-full rounded-md border-2 border-gray-300 bg-transparent px-3 py-2 text-sm placeholder-[#7E7D7E]
+        focus:outline-none focus:border-gray-600
+        dark:border-[rgba(255,255,255,0.06)] dark:focus:border-gray-400"
           />
 
           <PrimaryButton
